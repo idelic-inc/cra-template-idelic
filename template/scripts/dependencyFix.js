32155e6f9400fd349fe16e77ed9e7ded1de6d94e
Brian Duggan <bduggan@idelic.com>
2020-04-02 14:35:44 -0400

    Fixed packages and script

--- template/scripts/dependencyFix.js
+++ template/scripts/dependencyFix.js
@@ -3,6 +3,9 @@ const fileName = 'package.json';
 const file = require('../package.json');
 
 file.devDependencies = {
+  "@emotion/core": "^10.0.28",
+  "@emotion/styled": "^10.0.27",
+  "@svgr/webpack": "^5.3.0",
   "@testing-library/jest-dom": "5.3.0",
   "@testing-library/react": "^10.0.1",
   "@types/enzyme": "^3.10.5",
@@ -20,15 +23,22 @@ file.devDependencies = {
   "@types/uuid": "^3.4.5",
   "@typescript-eslint/eslint-plugin": "2.25.0",
   "@typescript-eslint/parser": "2.25.0",
+  "enzyme": "^3.11.0",
+  "enzyme-adapter-react-16": "^1.15.2",
   "enzyme-to-json": "^3.4.4",
+  "eslint": "^6.8.0",
   "eslint-config-airbnb-typescript": "^7.2.0",
   "eslint-config-prettier": "^6.10.1",
   "eslint-plugin-prettier": "^3.1.2",
   "eslint-plugin-simple-import-sort": "^5.0.2",
   "husky": "^4.2.3",
+  "jest": "^24.9.0",
   "jest-emotion": "^10.0.32",
+  "jest-environment-enzyme": "^7.1.2",
+  "jest-enzyme": "^7.1.2",
   "lint-staged": "10.1.0",
   "prettier": "^2.0.2",
+  "react-scripts": "^3.3.0",
   "react-test-renderer": "^16.12.0",
   "redux-mock-store": "^1.5.3",
   "ts-jest": "^24.2.0",
@@ -42,5 +52,5 @@ for(const dep in file.devDependencies) {
 fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
   if (err) return console.log(err);
 
-  console.log('Moving packages to devDependencies');
+  console.log('Moved packages to devDependencies');
 });
